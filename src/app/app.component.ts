f18a99a01db9a2133746a8bfbffaa4c5e718a4c8
Nutta Taprasert <s5811423115@gmail.com>
2018-07-23 13:37:48 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:String = 'My Angula Application';
+  version:Number = 1.0;
+  disabled:Boolean = true;
+
+  changeTitle(event){
+    console.log(event.target.value);
+    this.title = event.target.value;
+    //this.title = event.key;
+  }
+}
